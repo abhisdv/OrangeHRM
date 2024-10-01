98dca61ea2b640b93d972121706e56ccf8b1690a
admin <admin@SmartKyler>
2024-10-01 13:00:15 +0530

    build02

--- OrangeHRM/src/test/java/hrm_Admin/Add_user_admin.java
+++ OrangeHRM/src/test/java/hrm_Admin/Add_user_admin.java
@@ -8,6 +8,7 @@ public class Add_user_admin extends Base_class {
 	@Test
 	public void login() {
 		MethodRepo_admin mr = new MethodRepo_admin();
+		System.out.println("new implementation");
 		mr.login();
 	}
 }
---
+++ OrangeHRM/src/test/java/hrm_Admin/RemoveUser.java
@@ -0,0 +1,10 @@
+package hrm_Admin;
+
+import org.testng.annotations.Test;
+
+public class RemoveUser {
+	@Test
+	public void heelo() {
+		System.out.println("new class implemetaion");
+	}
+}
